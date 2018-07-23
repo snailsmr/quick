10a37fc3dae9073186a7d05bf5cc3f6bf12fae32
snail <185466981@qq.com>
2018-07-23 14:58:48 +0800

    本地部署版本，需还原数据库并且修改数据库连接信息0.0.2

---
+++ src/main/java/com/yhc/quick/SpringBootStartApplication.java
@@ -0,0 +1,12 @@
+package com.yhc.quick;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(QuickApplication.class);
+    }
+}
